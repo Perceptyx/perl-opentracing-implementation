315f06d0f8ed10b3d044638bfc50bc70912b3375
Theo van Hoesel <tvanhoesel@perceptyx.com>
2023-06-05 10:11:54 +0200

    Change the class method check to allow additional arguments

--- lib/OpenTracing/Implementation/Interface/Bootstrap.pm
+++ lib/OpenTracing/Implementation/Interface/Bootstrap.pm
@@ -1,11 +1,13 @@
 package OpenTracing::Implementation::Interface::Bootstrap;
 
 use Role::Declare::Should;
-use Types::Standard qw/ConsumerOf/;
+use Types::Standard qw/Any ConsumerOf Maybe Str/;
 
 
 
 class_method bootstrap_tracer(
+    Str $implementation_name,
+    Maybe [ Any ] @implementation_args,
 ) :Return ( ConsumerOf['OpenTracing::Interface::Tracer'] ) { }
 
 
